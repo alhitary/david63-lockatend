e0680cf8d31a150fde76306d86c0a675233f08fb
david63 <david.wood@pear-tree.org.uk>
2015-02-08 10:41:35 +0000

    UCP option added

--- david63/lockatend/event/listener.php
+++ david63/lockatend/event/listener.php
@@ -19,6 +19,35 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 */
 class listener implements EventSubscriberInterface
 {
+	/** @var \phpbb\config\config */
+	protected $config;
+
+	/** @var \phpbb\request\request */
+	protected $request;
+
+	/** @var \phpbb\template\twig\twig */
+	protected $template;
+
+	/** @var \phpbb\user */
+	protected $user;
+
+	/**
+	* Constructor for listener
+	*
+	* @param \phpbb\config\config		$config		Config object
+	* @param \phpbb\request\request		$request	Request object
+	* @param \phpbb\template\twig\twig	$template	Template object
+	* @param \phpbb\user                $user		User object
+	* @access public
+	*/
+	public function __construct(\phpbb\config\config $config, \phpbb\request\request $request, \phpbb\template\twig\twig $template, \phpbb\user $user)
+	{
+		$this->config	= $config;
+		$this->request	= $request;
+		$this->template	= $template;
+		$this->user		= $user;
+	}
+
 	/**
 	* Assign functions defined in this class to event listeners in the core
 	*
@@ -29,9 +58,72 @@ class listener implements EventSubscriberInterface
 	static public function getSubscribedEvents()
 	{
 		return array(
+			'core.user_setup'					=> 'load_user_data',
+			'core.ucp_prefs_view_data'			=> 'add_user_prefs',
+			'core.ucp_prefs_view_update_data'	=> 'update_user_prefs',
 			'core.viewforum_get_topic_ids_data'	=> 'update_viewforum_sql_ary',
 			'core.mcp_view_forum_modify_sql'	=> 'update_mcp_sql_ary',
 		);
+	}
+
+	/**
+	* Load the necessay data during user setup
+	*
+	* @param object $event The event object
+	* @return null
+	* @access public
+	*/
+	public function load_user_data($event)
+	{
+		// Load the language file
+		$lang_set_ext	= $event['lang_set_ext'];
+		$lang_set_ext[]	= array(
+			'ext_name' => 'david63/lockatend',
+			'lang_set' => 'lockatend',
+		);
+
+		$event['lang_set_ext'] = $lang_set_ext;
+	}
+
+	/**
+	* Add the necessay variables
+	*
+	* @param object $event The event object
+	* @return null
+	* @access public
+	*/
+	public function add_user_prefs($event)
+	{
+		$data = $event['data'];
+
+		$data = array_merge($data, array(
+			'lock_at_end' => $this->request->variable('lock_at_end', (!empty($user->data['user_lock_at_end'])) ? $user->data['user_lock_at_end'] : 0),
+		));
+
+		$event->offsetSet('data', $data);
+
+		$this->template->assign_vars(array(
+			'S_LOCK_AT_END'	=> $this->user->data['user_lock_at_end'],
+		));
+	}
+
+	/**
+	* Update the sql data
+	*
+	* @param object $event The event object
+	* @return null
+	* @access public
+	*/
+	public function update_user_prefs($event)
+	{
+		$sql_ary	= $event['sql_ary'];
+		$data		= $event['data'];
+
+		$sql_ary = array_merge($sql_ary, array(
+			'user_lock_at_end'	=> $data['lock_at_end'],
+		));
+
+		$event->offsetSet('sql_ary', $sql_ary);
 	}
 
 	/**
@@ -43,10 +135,13 @@ class listener implements EventSubscriberInterface
 	*/
 	public function update_viewforum_sql_ary($event)
 	{
-		$sql_ary			= $event['sql_ary'];
-		$store_reverse		= $event['store_reverse'];
-		$sql_sort_order		= $event['sql_sort_order'];
-		$event['sql_ary']	= str_replace($sql_sort_order, 't.topic_status ' . ((!$store_reverse) ? 'ASC' : 'DESC') . ', ' . $sql_sort_order, $sql_ary);
+		if ($this->user->data['user_lock_at_end'])
+		{
+			$sql_ary			= $event['sql_ary'];
+			$store_reverse		= $event['store_reverse'];
+			$sql_sort_order		= $event['sql_sort_order'];
+			$event['sql_ary']	= str_replace($sql_sort_order, 't.topic_status ' . ((!$store_reverse) ? 'ASC' : 'DESC') . ', ' . $sql_sort_order, $sql_ary);
+		}
 	}
 
 	/**
@@ -58,7 +153,10 @@ class listener implements EventSubscriberInterface
 	*/
 	public function update_mcp_sql_ary($event)
 	{
-		$sql			= $event['sql'];
-		$event['sql']	= str_replace('t.topic_last_post_time', 't.topic_status ASC, t.topic_last_post_time', $sql);
+		if ($this->user->data['user_lock_at_end'])
+		{
+			$sql			= $event['sql'];
+			$event['sql']	= str_replace('t.topic_last_post_time', 't.topic_status ASC, t.topic_last_post_time', $sql);
+		}
 	}
 }
--- david63/lockatend/migrations/version_1_0_0.php
+++ david63/lockatend/migrations/version_1_0_0.php
@@ -17,4 +17,32 @@ class version_1_0_0 extends \phpbb\db\migration\migration
 			array('config.add', array('lockatend_version', '1.0.0')),
 		);
 	}
+
+	public function update_schema()
+	{
+		return array(
+			'add_columns'	=> array(
+				$this->table_prefix . 'users'	=> array(
+					'user_lock_at_end'	=> array('BOOL', 1),
+				),
+			),
+		);
+	}
+
+	/**
+	* Drop the columns schema from the tables
+	*
+	* @return array Array of table schema
+	* @access public
+	*/
+	public function revert_schema()
+	{
+		return array(
+			'drop_columns'	=> array(
+				$this->table_prefix . 'users'	=> array(
+					'user_lock_at_end',
+				),
+			),
+		);
+	}
 }
